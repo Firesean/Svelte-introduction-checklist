dd2b68e17f60515da9d5593919352c9e851ca1c7
Sean Morgan <Firesean98@gmail.com>
2023-07-14 08:04:52 -0700

    Added Todo Input and stylization of components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
     import "../app.css";
+    import { onMount } from 'svelte';
+
+    let title = 'Checklist';
+        onMount(() => {
+        document.title = title; // Set the document title on component mount
+    });
 </script>
 
 <slot />
